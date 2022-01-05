1d4430928157f9eaf3d1c85372a300fed69cdbed
Jing Guo <dev.guoj@gmail.com>
2022-01-05 21:57:03 +0800

    irc-mode new file

---
+++ source/mode/irc.lisp
@@ -0,0 +1,11 @@
+;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
+;;;; SPDX-License-Identifier: BSD-3-Clause
+
+(uiop:define-package :nyxt/irc-mode
+  (:use :common-lisp :nyxt)
+  (:documentation "An IRC client within Nyxt."))
+(in-package :nyxt/irc-mode)
+
+(define-mode irc-mode ()
+  "Launches an IRC client in a new buffer."
+  )
