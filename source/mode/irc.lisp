3116b7308dc6833c37340b66408f7905651d6b3b
Jing Guo <dev.guoj@gmail.com>
2022-01-24 18:07:53 +0800

    mode/irc.lisp: Mode defined for IRC client and relevant command

--- source/mode/irc.lisp
+++ source/mode/irc.lisp
@@ -10,4 +10,55 @@
 
 (define-mode irc-mode ()
   "Launches an IRC client in a new buffer."
-  )
+  ((rememberable-p nil)
+   (keymap-scheme
+    (define-scheme "irc"
+      scheme:cua (list "return" 'send-command)
+      scheme:emacs (list)))
+   (style (theme:themed-css (theme *browser*)
+            (* :font-family "monospace,monospace")
+            (body :margin-right "0")
+            ("#container" :display "flex"
+                          :flex-flow "column"
+                          :height "100%"
+                          :color theme:text
+                          :background-color theme:background)
+            ("#input" :display "grid"
+                      :grid-template-columns "auto 1fr"
+                      :width "100%"
+                      :padding 0
+                      :margin 0
+                      :background-color theme:tertiary)
+            ("#input-buffer" :width "100%"
+                             :border "none"
+                             :outline "none"
+                             :padding "3px"
+                             :background-color theme:quaternary
+                             :autofocus "true")
+            ("#chat-history"
+             :font-size "12px"
+             :flex-grow "1"
+             :overflow-y "auto"
+             :overflow-x "auto")
+            ("#prompt" :padding-right "4px"
+                       :padding-left "4px"
+                       :line-height "30px"
+                       :color theme:background)
+            (ul :list-style "none"
+                :padding "0"
+                :margin "0")
+            (li :padding "2px"))
+          :documentation "The CSS applied to the IRC client when set up.")))
+
+(define-internal-page-command irc-client ()
+    (irc-buffer "*IRC Client*" 'irc-mode)
+  "Launch an IRC client."
+  (spinneret:with-html-string
+    (:head (:style (style (find-mode irc-buffer 'irc-mode))))
+    (:body
+     (:div :id "container"
+           (:div :id "chat-history" "")
+           (:div :id "input"
+                 (:span :id "prompt"
+                        (format nil ">"))
+                 (:input :type "text" :id "input-buffer"))))))
